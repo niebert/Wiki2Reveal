0242660bf875fdf22ae94d019ecfafd212a9179c
Engelbert Niehaus <niehaus@uni-landau.de>
2018-11-06 12:28:02 +0100

    Replacement of Wiki References in Text

--- docs/js/wikiconvert.js
+++ docs/js/wikiconvert.js
@@ -1085,12 +1085,12 @@ this.process_normal = function(wikitext) {
 	  //-------------------------------------------------------
 	  if (pWikiCode) {
       	// (1) References without a citaion label
-      	pWikiCode = pWikiCode.replace(/ ?<ref>([\s\S]{0,1000}?)<\/ref> ?/gi,"");
+      	pWikiCode = pWikiCode.replace(/ ?<ref>([\s\S]{0,1000}?)<\/ref> ?/gi," ");
       	// (2) Cite a reference by a label WITHOUT reference
       	// replace <ref name="my book label"/> by "___CITE_7238234792_my_book_label___"
-      	pWikiCode = pWikiCode.replace(/ ?<ref[\s]+name=["']([^"'])["'][^>]{0,200}?\/> ?/gi,"");
+      	pWikiCode = pWikiCode.replace(/ ?<ref[\s]+name=["']([^"'])["'][^>]{0,200}?\/> ?/gi," ");
       	// (3) Reference with citation label that is used multiple time in a document by (2)
-      	pWikiCode = pWikiCode.replace(/ ?<ref [\s]+name=["']([^"'])["'][^>]{0,200}?>([\s\S]{0,1000}?)<\/ref> ?/gi,"");
+      	pWikiCode = pWikiCode.replace(/ ?<ref [\s]+name=["']([^"'])["'][^>]{0,200}?>([\s\S]{0,1000}?)<\/ref> ?/gi," ");
       	pWikiCode = pWikiCode.replace(/\{\{[^}]\}\}/g, '');
 	  } else {
       	pwiki = " ";

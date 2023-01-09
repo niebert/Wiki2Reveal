bdcaa57f38d9d6fb6759d91d1a3e5e172dc65f24
Engelbert Niehaus <niehausbert@gmail.com>
2023-01-09 12:31:55 +0100

    Add files via upload

---
+++ docs/db/footer4lang.js
@@ -0,0 +1,67 @@
+vDataJSON.footer4lang = {
+  en:`
+== Page Information ==
+You can display this page as '''[https://niebert.github.io/Wiki2Reveal/wiki2___SLIDETYPE___.html?domain=___DOMAIN___&title=___TITLE_ENCODED___&author=___COURSE_ENCODED___&language=___LANGUAGE___&audioslide=___AUDIO___&shorttitle=___SHORT_TITLE_ENCODED___&coursetitle=___COURSE_TITLE_ENCODED___ Wiki2Reveal slides]'''
+
+=== Wiki2Reveal ===
+The '''[https://niebert.github.io/Wiki2Reveal/index.html?domain=___DOMAIN___&title=___TITLE_ENCODED___&author=___COURSE_ENCODED___&language=___LANGUAGE___&audioslide=___AUDIO___&shorttitle=___SHORT_TITLE_ENCODED___&coursetitle=___COURSE_TITLE_ENCODED___ Wiki2Reveal slides]''' were created for the '''[https://___LANGUAGE___.wikiversity.org/wiki/___COURSE_ENCODED___ ___COURSE___]'''' and the Link for the [[v:en:Wiki2Reveal|Wiki2Reveal Slides]] was created with the [https://niebert.github.io/Wiki2Reveal/ link generator].
+<!--
+* Contents of the page are based on:
+** [https://___LANGUAGE___.wikipedia.org/wiki/___TITLE_ENCODED___ https://___LANGUAGE___.___DOMAIN___.org/wiki/___TITLE_ENCODED___]
+-->
+* [https://___LANGUAGE___.___DOMAIN___.org/wiki/___TITLE_ENCODED___ This page] is designed as a [https://en.wikiversity.org/wiki/PanDocElectron-Presentation PanDocElectron-SLIDE] document type.
+* Source: ___DOMAINNAME___  https://___LANGUAGE___.___DOMAIN___.org/wiki/___TITLE_ENCODED___
+* see [[v:en:Wiki2Reveal|Wiki2Reveal]] for the functionality of [https://niebert.github.io/Wiki2Reveal/index.html?domain=___DOMAIN___&title=___TITLE_ENCODED___&author=___COURSE_ENCODED___&language=___LANGUAGE___&audioslide=___AUDIO___&shorttitle=___SHORT_TITLE_ENCODED___&coursetitle=___COURSE_TITLE_ENCODED___ Wiki2Reveal].
+<!-- * Next contents of the course are [[]] -->;
+
+[[Category:Wiki2Reveal]]`,
+  de:`
+== Seiteninformation ==
+Diese Lernresource können Sie als '''[https://niebert.github.io/Wiki2Reveal/wiki2___SLIDETYPE___.html?domain=___DOMAIN___&title=___TITLE_ENCODED___&author=___COURSE_ENCODED___&language=___LANGUAGE___&audioslide=___AUDIO___&shorttitle=___SHORT_TITLE_ENCODED___&coursetitle=___COURSE_TITLE_ENCODED___ Wiki2Reveal-Foliensatz]''' darstellen.
+
+=== Wiki2Reveal ===
+
+Dieser '''[https://niebert.github.io/Wiki2Reveal/wiki2___SLIDETYPE___.html?domain=___DOMAIN___&title=___TITLE_ENCODED___&author=___COURSE_ENCODED___&language=___LANGUAGE___&audioslide=___AUDIO___&shorttitle=___SHORT_TITLE_ENCODED___&coursetitle=___COURSE_TITLE_ENCODED___ Wiki2Reveal Foliensatz]''' wurde für den Lerneinheit '''[https://___LANGUAGE___.wikiversity.org/wiki/___COURSE_ENCODED___ ___COURSE___]'''' erstellt der Link für die [[v:en:Wiki2Reveal|Wiki2Reveal-Folien]] wurde mit dem  [https://niebert.github.io/Wiki2Reveal/ Wiki2Reveal-Linkgenerator] erstellt.
+<!--
+* Die Inhalte der Seite basieren auf den folgenden Inhalten:
+** [https://___LANGUAGE___.wikipedia.org/wiki/___TITLE_ENCODED___ https://___LANGUAGE___.___DOMAIN___.org/wiki/___TITLE_ENCODED___]
+-->
+* [https://___LANGUAGE___.___DOMAIN___.org/wiki/___TITLE_ENCODED___ Die Seite] wurde als Dokumententyp [https://___LANGUAGE___.wikiversity.org/wiki/PanDocElectron-Presentation PanDocElectron-SLIDE] erstellt.
+* Link zur Quelle in  ___DOMAINNAME___:  https://___LANGUAGE___.___DOMAIN___.org/wiki/___TITLE_ENCODED___
+* siehe auch weitere Informationen zu [[v:en:Wiki2Reveal|Wiki2Reveal]] und unter [https://niebert.github.io/Wiki2Reveal/index.html?domain=___DOMAIN___&title=___TITLE_ENCODED___&author=___COURSE_ENCODED___&language=___LANGUAGE___&audioslide=___AUDIO___&shorttitle=___SHORT_TITLE_ENCODED___&coursetitle=___COURSE_TITLE_ENCODED___ Wiki2Reveal-Linkgenerator].
+<!-- * Nächster Inhalt des Kurses ist [[]] -->;
+
+[[Category:Wiki2Reveal]]`,
+  fr:`
+== Informations de Page ==
+
+=== Wiki2Reveal ===
+Ce jeu de '''[https://niebert.github.io/Wiki2Reveal/wiki2___SLIDETYPE___.html?domain=___DOMAIN___&title=___TITLE_ENCODED___&author=___COURSE_ENCODED___&language=___LANGUAGE___&audioslide=___AUDIO___ diapositives Wiki2Reveal]''' a été créé pour la session '''[https://___LANGUAGE___.wikiversity.org/wiki/___COURSE_ENCODED___ ___COURSE___]''' et le lien pour les diapositives Wiki2Reveal a été créé à l'aide du [https://niebert.github.io/Wiki2Reveal/ générateur de liens Wiki2Reveal].
+<!--
+* Le contenu de la page est basé sur le contenu suivant:
+** [https://___LANGUAGE___.wikipedia.org/wiki/___TITLE_ENCODED___ https://___LANGUAGE___.wikipedia.org/wiki/___TITLE_ENCODED___]
+-->
+* [https://___LANGUAGE___.___DOMAIN___.org/wiki/___TITLE_ENCODED___ Le site] a été créé comme un type de document [https://en.wikiversity.org/wiki/PanDocElectron-Presentation PanDocElectron-SLIDE].
+* Lien vers la source  ___DOMAINNAME___: https://___LANGUAGE___.___DOMAIN___.org/wiki/___TITLE_ENCODED___
+* Voir les détails sur [[v:fr:Wiki2Reveal|Wiki2Reveal]] et [https://niebert.github.io/Wiki2Reveal/index.html?domain=___DOMAIN___&title=___TITLE_ENCODED___&author=___COURSE_ENCODED___&language=___LANGUAGE___&audioslide=___AUDIO___&shorttitle=___SHORT_TITLE_ENCODED___&coursetitle=___COURSE_TITLE_ENCODED___ générateur Wiki2Reveal-Link].
+<!-- * Le contenu suivant du cours est [[]] -->
+
+[[Category:Wiki2Reveal]]`,
+  es:`
+
+== Información de la Página ==
+
+=== Wiki2Reveal ===
+Este conjunto de '''[https://niebert.github.io/Wiki2Reveal/wiki2___SLIDETYPE___.html?domain=___DOMAIN___&title=___TITLE_ENCODED___&author=___COURSE_ENCODED___&language=___LANGUAGE___&audioslide=___AUDIO___&shorttitle=___SHORT_TITLE_ENCODED___&coursetitle=___COURSE_TITLE_ENCODED___ diapositivas de Wiki2Reveal]''' se creó para la sesión '''[https://___LANGUAGE___.wikiversity.org/wiki/___COURSE_ENCODED___ ___CURSO___]''', y el enlace para las diapositivas de Wiki2Reveal se creó con el [https://niebert.github.io/Wiki2Reveal/ generador de enlaces de Wiki2Reveal].
+<!--
+* El contenido de la página se basa en el siguiente contenido:
+** [https://___LANGUAGE___.wikipedia.org/wiki/___TITLE_ENCODED___ https://___LANGUAGE___.wikipedia.org/wiki/___TITLE_ENCODED___]
+-->
+* [https://___LANGUAGE___.___DOMAIN___.org/wiki/___TITLE_ENCODED___ La página] se creó como tipo de documento [https://en.wikiversity.org/wiki/PanDocElectron-Presentation PanDocElectron-SLIDE].
+* Enlace a la fuente en  ___DOMAINNAME___: https://___LANGUAGE___.___DOMAIN___.org/wiki/___TITLE_ENCODED___
+* Consulte también más información sobre [[v:en:Wiki2Reveal|Wiki2Reveal]] y [https://niebert.github.io/Wiki2Reveal/index.html?domain=___DOMAIN___&title=___TITLE_ENCODED___&author=___COURSE_ENCODED___&language=___LANGUAGE___&audioslide=___AUDIO___&shorttitle=___SHORT_TITLE_ENCODED___&coursetitle=___COURSE_TITLE_ENCODED___ Wiki2Reveal link generator].
+<!-- * El siguiente contenido del curso es [[]] -->
+
+[[Categoría: Wiki2Reveal]]`
+
+};
---
+++ docs/db/page4demo.js
@@ -0,0 +1,79 @@
+vDataJSON.page4demo = {
+  en:`
+== Page Information ==
+== Introduction ==
+This page can be displayed as '''[https://niebert.github.io/Wiki2Reveal/wiki2___SLIDETYPE___.html?domain=___DOMAIN___&title=___TITLE_ENCODED___&author=___COURSE_ENCODED___&language=___LANGUAGE___&audioslide=___AUDIO___&shorttitle=___SHORT_TITLE_ENCODED___&coursetitle=___COURSE_TITLE_ENCODED___ Wiki2Reveal slides]'''.
+Single sections are regarded as slides and modifications on the slides will immediately affect the content of the slides.
+
+== Objective ==
+This learning resource in Wikiversity has the objective to ...
+
+== Target Group ==
+The target group of the learning resource is
+
+== Learning Tasks / Activities ==
+
+== References ==
+<references/>
+
+=== See also ===
+* [[Wiki2Reveal]]`,
+  de:`
+== Seiteninformation ==
+== Einleitung ==
+Diese Seite kann als '''[https://niebert.github.io/Wiki2Reveal/wiki2___SLIDETYPE___.html?domain=___DOMAIN___&title=___TITLE_ENCODED___&author=___COURSE_ENCODED___&language=___LANGUAGE___&audioslide=___AUDIO___&shorttitle=___SHORT_TITLE_ENCODED___&coursetitle=___COURSE_TITLE_ENCODED___ Wiki2Reveal Folien]''' angezeigt werden.
+Einzelne Abschnitte werden als Folien betrachtet und Änderungen an den Folien wirken sich sofort auf den Inhalt der Folien aus.
+
+== Zielsetzung ==
+Diese Lernressource in der Wikiversity hat das Ziel, ...
+
+== Zielgruppe ==
+Die Zielgruppe der Lernressource ist
+
+== Aufgaben für Lernende / Studierende ==
+
+== Literatur/Quellennachweise ==
+<references/>
+
+
+== Siehe auch ==
+* [[Wiki2Reveal]]`,
+  fr:`
+== Introduction ==
+Cette page peut être affichée comme '''[https://niebert.github.io/Wiki2Reveal/wiki2___SLIDETYPE___.html?domain=___DOMAIN___&title=___TITLE_ENCODED___&author=___COURSE_ENCODED___&language=___LANGUAGE___&audioslide=___AUDIO___&shorttitle=___SHORT_TITLE_ENCODED___&coursetitle=___COURSE_TITLE_ENCODED___ Wiki2Reveal slides]'''.
+Les sections individuelles sont considérées comme des diapositives et les modifications sur les diapositives affecteront immédiatement le contenu des diapositives.
+
+== Objectif ==
+Cette ressource pédagogique de Wikiversity a pour objectif de ...
+
+== Groupe cible ==
+Le groupe cible de cette ressource pédagogique est
+
+== Tâches pour les apprenants / étudiants
+
+== Sources de littérature ==
+<references/>
+
+== Voir aussi ==
+* [[Wiki2Reveal]]`,
+  es:`
+
+== Introducción ==
+Esta página puede mostrarse como '''[https://niebert.github.io/Wiki2Reveal/wiki2___SLIDETYPE___.html?domain=___DOMAIN___&title=___TITLE_ENCODED___&author=___COURSE_ENCODED___&language=___LANGUAGE___&audioslide=___AUDIO___&shorttitle=___SHORT_TITLE_ENCODED___&coursetitle=___COURSE_TITLE_ENCODED___ Wiki2Reveal slides]'''.
+Las secciones individuales se consideran diapositivas y las modificaciones en las diapositivas afectarán inmediatamente al contenido de las mismas.
+
+== Objetivo ==
+Este recurso de aprendizaje en la Wikiversidad tiene como objetivo...
+
+== Grupo destinatario ==
+El grupo objetivo del recurso de aprendizaje es
+
+== Tareas para los alumnos / estudiantes ==
+
+== Fuentes bibliográficas ==
+<references/>
+
+== Ver también ==
+* [[Wiki2Reveal]]`
+
+};

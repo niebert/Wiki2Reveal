3b44f863ec4ca073bd981b663a20c8d4a7e08a9f
Engelbert Niehaus <niehausbert@gmail.com>
2024-06-01 11:44:58 +0200

    Add files via upload

---
+++ docs/db/header4lang.js
@@ -0,0 +1,52 @@
+vDataJSON.header4lang = {
+    en:`
+  == Introduction ==
+  This page about ''___TITLE___'' can be displayed as '''[https://niebert.github.io/Wiki2Reveal/wiki2___SLIDETYPE___.html?domain=___DOMAIN___&title=___TITLE_ENCODED___&author=___COURSE_ENCODED___&language=___LANGUAGE___&audioslide=___AUDIO___&shorttitle=___SHORT_TITLE_ENCODED___&coursetitle=___COURSE_TITLE_ENCODED___ Wiki2Reveal slides]'''.
+  Single sections are regarded as slides and modifications on the slides will immediately affect the content of the slides.
+  The following aspects of ''___TITLE___'' are considered in detail:
+  * (1)
+  * (2)
+  * (3)
+
+  == Objective ==
+  This learning resource about ''___TITLE___'' in Wikiversity has the objective to ...
+
+  == Learning Prerequisites ==
+  This [[Open Educational Resources|learning resource]] about ''___TITLE___''
+
+`,
+    de:`
+  == Einleitung ==
+  Diese Seite zum Thema ''___TITLE___'' kann als '''[https://niebert.github.io/Wiki2Reveal/wiki2___SLIDETYPE___.html?domain=___DOMAIN___&title=___TITLE_ENCODED___&author=___COURSE_ENCODED___&language=___LANGUAGE___&audioslide=___AUDIO___&shorttitle=___SHORT_TITLE_ENCODED___&coursetitle=___COURSE_TITLE_ENCODED___ Wiki2Reveal Folien]''' angezeigt werden.
+  Einzelne Abschnitte werden als Folien betrachtet und Änderungen an den Folien wirken sich sofort auf den Inhalt der Folien aus.
+  Dabei werden die folgenden Teilaspekte im Detail behandelt:
+  * (1)
+  * (2)
+  * (3)
+
+  == Zielsetzung ==
+  Diese Lernressource zu ''___TITLE___'' in der Wikiversity hat das Ziel, ...
+
+  == Lernvoraussetzungen ==
+  Die Lernressource zum Thema  ''___TITLE___'' hat die folgenden Lernvoraussetzungen, die zum Verständnis der nachfolgenden Ausführungen hilfreich bzw. notwendig sind.
+
+`,
+    fr:`
+  == Introduction ==
+  Cette page peut être affichée comme '''[https://niebert.github.io/Wiki2Reveal/wiki2___SLIDETYPE___.html?domain=___DOMAIN___&title=___TITLE_ENCODED___&author=___COURSE_ENCODED___&language=___LANGUAGE___&audioslide=___AUDIO___&shorttitle=___SHORT_TITLE_ENCODED___&coursetitle=___COURSE_TITLE_ENCODED___ Wiki2Reveal slides]'''.
+  Les sections individuelles sont considérées comme des diapositives et les modifications sur les diapositives affecteront immédiatement le contenu des diapositives.
+
+  == Objectif ==
+  Cette ressource pédagogique de Wikiversity a pour objectif de ...
+`,
+    es:`
+
+  == Introducción ==
+  Esta página puede mostrarse como '''[https://niebert.github.io/Wiki2Reveal/wiki2___SLIDETYPE___.html?domain=___DOMAIN___&title=___TITLE_ENCODED___&author=___COURSE_ENCODED___&language=___LANGUAGE___&audioslide=___AUDIO___&shorttitle=___SHORT_TITLE_ENCODED___&coursetitle=___COURSE_TITLE_ENCODED___ Wiki2Reveal slides]'''.
+  Las secciones individuales se consideran diapositivas y las modificaciones en las diapositivas afectarán inmediatamente al contenido de las mismas.
+
+  == Objetivo ==
+  Este recurso de aprendizaje en la Wikiversidad tiene como objetivo...
+`
+
+  };
